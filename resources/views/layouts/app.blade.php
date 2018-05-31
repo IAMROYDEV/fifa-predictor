d6b94685b2bd7935540764d59f9bb15d55c98e98
Santosh <santosh@niiu.de>
2018-05-31 17:32:52 +0530

    add logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,12 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{asset('img/logo.png')}}" width="15%">
+                    <span style="
+                        font-family: cursive;
+                        font-size: xx-large;
+                        font-weight: bold;
+                    ">fifa8teen</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
