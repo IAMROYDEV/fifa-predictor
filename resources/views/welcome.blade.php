af2ceb0e64bcc48a13aab7c26174fd7a994f821c
Santosh <santosh@niiu.de>
2018-05-31 17:59:27 +0530

    front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,6 +40,12 @@
                 right: 10px;
                 top: 18px;
             }
+            
+            .top-left {
+                position: absolute;
+                left: 10px;
+                top: 18px;
+            }
 
             .content {
                 text-align: center;
@@ -66,6 +72,14 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+            <div class="top-left links">
+                <img src="{{asset('img/logo.png')}}" width="15%" style="float: left">
+                    <span style="
+                        font-family: cursive;
+                        font-size: xx-large;
+                        font-weight: bold;
+                    ">fifa8teen</span>
+            </div>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,15 +93,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <img src="{{asset('img/football-ball.png')}}">
+                    
                 </div>
             </div>
         </div>
