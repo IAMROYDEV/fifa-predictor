0674da09f17c9a10a690cba22ef2639ab5c74fb9
clinton <clinton@goodnity.com>
2018-06-09 21:10:52 +0530

    Auth required for match controller.

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -9,6 +9,16 @@ use App\UserMatchPrediction;
 
 class MatchController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request, $worldCupId)
     {
         $stageSelected= 0;
