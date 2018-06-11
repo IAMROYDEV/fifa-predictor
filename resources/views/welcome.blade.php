c9287fb5a1c9fa24b2fc1fa0ed1d1db17067f9d7
Santosh <santosh@niiu.de>
2018-06-11 19:42:07 +0530

    login button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,6 +68,63 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            
+            /* Shared */
+            .loginBtn a {
+                text-decoration: none;
+                color: #fff;
+            }
+            .loginBtn
+            {
+              box-sizing: border-box;
+              position: relative;
+              /* width: 13em;  - apply for fixed size */
+              margin: 0.2em;
+              padding: 0 15px 0 46px;
+              border: none;
+              text-align: left;
+              line-height: 34px;
+              white-space: nowrap;
+              border-radius: 0.2em;
+              font-size: 16px;
+              color: #FFF;
+            }
+            .loginBtn:before {
+              content: "";
+              box-sizing: border-box;
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 34px;
+              height: 100%;
+            }
+            .loginBtn:focus {
+              outline: none;
+            }
+            .loginBtn:active {
+              box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
+            }
+
+
+            /* Facebook */
+            .loginBtn--facebook {
+              background-color: #4C69BA;
+              background-image: linear-gradient(#4C69BA, #3B55A0);
+              /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+              text-shadow: 0 -1px 0 #354C8C;
+            }
+            .loginBtn--facebook:before {
+              border-right: #364e92 1px solid;
+              background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
+            }
+            .loginBtn--facebook:hover,
+            .loginBtn--facebook:focus {
+              background-color: #5B7BD5;
+              background-image: linear-gradient(#5B7BD5, #4864B1);
+            }
+
+
+           
             body {
                 background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url("/img/background.jpg");
                 background-size: cover;
@@ -86,12 +143,21 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/dashboard') }}">Home</a>
                     @endauth
+                    <a href="{{ url('/rules') }}">Rules</a>
+                    <a href="{{ url('/faq') }}">FAQ</a>
                 </div>
             @endif
 
             <div class="content">
+                @auth
+                
+                @else
+                  <button class="loginBtn loginBtn--facebook">
+                      <a href="{{url('/redirect')}}" >Login with Facebook</a>
+                  </button>
+                @endauth
             </div>
         </div>
     </body>
