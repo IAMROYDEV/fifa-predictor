3f0b05c13ce8f6f0348505e13a03b27ca46f5c99
Santosh <santosh@niiu.de>
2018-06-11 19:21:33 +0530

    auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,9 +87,6 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
             @endif
