0166e3242e433bee6520ffc30acc35b024681bd2
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2018-06-12 09:23:43 +0530

    added slack service

--- app/Services/SocialFacebookAccountService.php
+++ app/Services/SocialFacebookAccountService.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Services;
+
 use App\SocialFacebookAccount;
 use App\User;
 use Laravel\Socialite\Contracts\User as ProviderUser;
 
-class SocialFacebookAccountService {
+class SocialFacebookAccountService
+{
     public function createOrGetUser(ProviderUser $providerUser)
     {
         $account = SocialFacebookAccount::whereProvider('facebook')
@@ -15,7 +17,6 @@ class SocialFacebookAccountService {
         if ($account) {
             return $account->user;
         } else {
-
             $account = new SocialFacebookAccount([
                 'provider_user_id' => $providerUser->getId(),
                 'provider' => 'facebook'
@@ -24,18 +25,17 @@ class SocialFacebookAccountService {
             $user = User::whereEmail($providerUser->getEmail())->first();
 
             if (!$user) {
-
                 $user = User::create([
                     'email' => $providerUser->getEmail(),
                     'name' => $providerUser->getName(),
-                    'password' => md5(rand(1,10000)),
+                    'password' => md5(rand(1, 10000)),
                     'avatar' => $providerUser->getAvatar()
                 ]);
             }
 
             $account->user()->associate($user);
             $account->save();
-
+            SlackService::sendMessage("new user registered \nname *{$user->name}*\nemail{$user->email}");
             return $user;
         }
     }
