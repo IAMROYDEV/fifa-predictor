f9fd247d5fa7644bf4e1ea44b71aad8e7fa23b5f
clinton <clinton@goodnity.com>
2018-06-14 10:52:31 +0530

    Changed the layout of the dashboard.

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -5,6 +5,14 @@
     <div class="row">
         <div class="col-md-6">
             <div class="col-md-12">
+                @include('user.predictions.current-match', ['currentMatch' => $currentMatch, 'currentMatchPrediction' => $currentMatchPrediction])
+            </div>
+            <div class="col-md-12">
+                @include('user.predictions.squad')
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="col-md-12">
                 @include('user.predictions.favourite-team')
             </div>
             <div class="col-md-12">
@@ -18,14 +26,6 @@
             </div>
             <div class="col-md-12">
                 @include('user.predictions.worldcup-winner')
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="col-md-12">
-                @include('user.predictions.current-match', ['currentMatch' => $currentMatch, 'currentMatchPrediction' => $currentMatchPrediction])
-            </div>
-            <div class="col-md-12">
-                @include('user.predictions.squad')
             </div>
         </div>
     </div>
