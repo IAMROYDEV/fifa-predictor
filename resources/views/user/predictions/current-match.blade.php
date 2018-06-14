41130825b55e2ccad3005912a81a20d056cf188c
clinton <clinton@goodnity.com>
2018-06-15 01:42:18 +0530

    Upcoming match prediction fix.

--- resources/views/user/predictions/current-match.blade.php
+++ resources/views/user/predictions/current-match.blade.php
@@ -1,6 +1,10 @@
 <div class="card">
     <div class="card-header">
+        @if($currentMatch)
+        Upcoming match Predictions
+        @else
         Match Predictions
+        @endif
         <i class="fe fe-help-circle button-right" data-toggle="tooltip" data-placement="top" title="Predict daily match scores and add points to your tally. Match score predictor is locked 1 hour before the match starts.!!"></i>
     </div>
     <div class="card-body text-center">
